5dafc3c1d46c46cc1dbcccc8636e97a10dce5bac
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-07-16 05:23:12 +0100

    Respect PreferMultiShiny setting from RandoPlus

--- TrandoPlus/RestrictedRoomRando/RestrictedRoomRandoManager.cs
+++ TrandoPlus/RestrictedRoomRando/RestrictedRoomRandoManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RandomizerMod.RC;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,15 @@ namespace TrandoPlus.RestrictedRoomRando
         {
             RequestMaker.Hook();
             RandomizerMod.Logging.LogManager.AddLogger(new ActiveScenesLogger());
+            RandoController.OnExportCompleted += BeforeGameStart;
+        }
+
+        private static void BeforeGameStart(RandoController rc)
+        {
+            if (TrandoPlus.GS.LimitedRoomRando && RandoPlus.RandoPlus.GS.PreferMultiShiny)
+            {
+                RandoPlus.AreaRestriction.AreaRestriction.PreventMultiChests();
+            }
         }
     }
 }
