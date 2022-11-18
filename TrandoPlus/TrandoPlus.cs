cb04976ca7b9b8bd2a52b9f26923f95863acfc94
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-11-18 23:20:51 +0000

    Actually run the new code

--- TrandoPlus/TrandoPlus.cs
+++ TrandoPlus/TrandoPlus.cs
@@ -38,8 +38,9 @@ namespace TrandoPlus
             bool rando = ModHooks.GetMod("Randomizer 4") is Mod;
 
             if (rando) MenuHolder.Hook();
-            if (rando) DoorRandoAdder.Hook();
-            if (rando) DropRandoAdder.Hook();
+            // if (rando) DoorRandoAdder.Hook();
+            // if (rando) DropRandoAdder.Hook();
+            if (rando) ExtraRandomizedTransitions.ExtraTransitionRequest.Hook();
             if (rando) ConditionManager.Hook();
             if (rando) RestrictedRoomRando.RoomRemovalManager.Hook();
 
